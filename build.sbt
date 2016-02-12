d7e1b01873833719e390d4504258c80998b84e6e
Octavian Geagla <ogeagla@gmail.com>
2016-02-12 16:42:13 -0700

    working threeJS example

--- build.sbt
+++ build.sbt
@@ -13,6 +13,8 @@ persistLauncher in Compile := true
 
 persistLauncher in Test := false
 
+skip in packageJSDependencies := false
+
 testFrameworks += new TestFramework("utest.runner.Framework")
 
 libraryDependencies ++= Seq(
@@ -20,6 +22,12 @@ libraryDependencies ++= Seq(
     "com.lihaoyi" %%% "utest" % "0.3.0" % "test"
 )
 
+resolvers += sbt.Resolver.bintrayRepo("denigma", "denigma-releases") //add resolver
+jsDependencies += "org.webjars" % "three.js" % "r74" / "three.js"
+libraryDependencies += "org.denigma" %%% "threejs-facade" % "0.0.71-0.1.5" //add dependency
+
 bootSnippet := "example.ScalaJSExample().main(document.getElementById('canvas'));"
 
 updateBrowsers <<= updateBrowsers.triggeredBy(fastOptJS in Compile)
+
+scalaJSUseRhino in Global := false
--- src/main/scala/example/ScalaJSExample.scala
+++ src/main/scala/example/ScalaJSExample.scala
@@ -1,5 +1,6 @@
 package example
 
+import org.denigma.threejs._
 import org.scalajs.dom
 
 import scala.scalajs.js
@@ -7,9 +8,45 @@ import scala.scalajs.js
 object ScalaJSExample extends js.JSApp {
   def main(): Unit = {
     val paragraph = dom.document.createElement("p")
-    paragraph.innerHTML = "<strong>It works!  For real!</strong>"
+    paragraph.innerHTML = "<strong>It works!  For real! Again!</strong>"
     dom.document.getElementById("playground").appendChild(paragraph)
 
+    val scene = new Scene
+
+    val camera = new PerspectiveCamera(75, dom.window.innerWidth / dom.window.innerHeight, 0.1, 1000)
+
+    val renderer = new WebGLRenderer()
+
+    renderer.setSize(dom.window.innerWidth, dom.window.innerHeight)
+
+    dom.document.body.appendChild(renderer.domElement)
+
+    val geometry = new BoxGeometry(1, 1, 1)
+
+    val material: MeshBasicMaterial = new MeshBasicMaterial()
+//    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+    val cube = new Mesh(geometry, material)
+
+    scene.add(cube)
+
+    camera.position.z = 5
+
+    cube.rotateX(0.1)
+    cube.rotateY(0.1)
+
+    renderer.render(scene , camera)
+//
+//    var render = function () {
+//      requestAnimationFrame( render );
+//
+//      cube.rotation.x += 0.1;
+//      cube.rotation.y += 0.1;
+//
+//      renderer.render(scene, camera);
+//    };
+//
+//    render();
 
   }
 
